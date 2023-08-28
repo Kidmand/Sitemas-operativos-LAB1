358e06ff5d04e0b71987629ab063cb1bbc5fd740
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-08-28 13:13:08 -0300

    nuevos test de comando simple

--- nuestros_test/test_command.c
+++ nuestros_test/test_command.c
@@ -9,7 +9,24 @@ int main(int argc, char *argv[])
 {
     printf("Esto es un test de command.c\n");
     scommand comand = scommand_new();
-    scommand_push_back(comand, strdup("ramiro"));
+    printf("Es vacia : %d - El tamaño es: %u \n", scommand_is_empty(comand), scommand_length(comand));
+    scommand_push_back(comand, strdup("ls"));
+    printf("El primer elemento es: %s\n", scommand_front(comand));
+    printf("Es vacia : %d - El tamaño es: %u \n", scommand_is_empty(comand), scommand_length(comand));
+
+    scommand_pop_front(comand);
+    printf("Es vacia : %d - El tamaño es: %u \n", scommand_is_empty(comand), scommand_length(comand));
+
+    scommand_set_redir_in(comand, strdup("./in"));
+    scommand_set_redir_out(comand, strdup("./out"));
+    printf("La entrada es: %s\n", scommand_get_redir_in(comand));
+    printf("La salida es: %s\n", scommand_get_redir_out(comand));
+
+    scommand_push_back(comand, strdup("ls"));
+    scommand_push_back(comand, strdup("-l"));
+    scommand_push_back(comand, strdup("xd"));
+    printf("El comando completo es : %s \n", scommand_to_string(comand));
+
     comand = scommand_destroy(comand);
     return EXIT_SUCCESS;
 }
