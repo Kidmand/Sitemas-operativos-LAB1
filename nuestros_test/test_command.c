3cc6c84026cb9ccf31def21dd59f0a8f7daa3f18
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-08-28 10:55:06 -0300

    test de command

--- nuestros_test/test_command.c
+++ nuestros_test/test_command.c
@@ -7,6 +7,8 @@
 int main(int argc, char *argv[])
 {
     printf("Esto es un test de command.c\n");
-
+    scommand comand = scommand_new();
+    scommand_push_back(comand, "ramiro");
+    comand = scommand_destroy(comand);
     return EXIT_SUCCESS;
 }
