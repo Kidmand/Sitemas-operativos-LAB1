45073031839954a29f911a84f19d48d7444a3753
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-08-28 09:37:33 -0300

    Agregamos nuestos test

---
+++ nuestros_test/test_command.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../command.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Esto es un test de command.c\n");
+
+    return EXIT_SUCCESS;
+}
