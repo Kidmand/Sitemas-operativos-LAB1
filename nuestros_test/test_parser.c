ba0728df5917c5de6d0c56f94c3e444698e7fe7a
Usuario Visita <visitante@labo39.famaf.unc.edu.ar>
2023-08-29 16:30:35 -0300

    aprender del parser

---
+++ nuestros_test/test_parser.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "../parser.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Entendiendo como funciona.");
+
+    return EXIT_SUCCESS;
+}
