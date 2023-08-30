4f0dc68bcc6c568f2b88944b32008c592bd36850
vmatiasw <matias.viola@mi.unc.edu.ar>
2023-08-30 19:24:40 -0300

    Casi termino de builtin, test

--- nuestros_test/test_builtin.c
+++ nuestros_test/test_builtin.c
@@ -20,5 +20,24 @@ int main(int argc, char *argv[])
     b = builtin_is_internal(comand);
     printf("%s es un comando interno: %d \n", scommand_front(comand), b);
 
+    pipeline p = pipeline_new();
+    pipeline_push_back(p,comand); 
+    printf("El pipeline %s tiene solo un elemento y este se corresponde a un comando interno: %d \n",pipeline_to_string(p) ,builtin_alone(p));
+
+    // Probamos echo
+    scommand_pop_front(comand);
+    scommand_push_back(comand, strdup("echo"));
+    scommand_push_back(comand, strdup("hello word!!"));
+    builtin_run(comand);
+
+
+    // Probamos exit
+    scommand_pop_front(comand);
+    scommand_push_back(comand, strdup("exit"));
+    printf("%s \n", scommand_front(comand));
+    builtin_run(comand);
+
+    exit(0); // no hace nada ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯
+
     return EXIT_SUCCESS;
 }
