a49e407d4436bbe616a13792904e8708c497d1e4
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-08-30 18:11:38 -0300

    se empezo con la implementacion del parsing

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "parsing.h"
 #include "parser.h"
@@ -7,36 +8,73 @@
 
 static scommand parse_scommand(Parser p)
 {
-    /* Devuelve NULL cuando hay un error de parseo */
-    return NULL;
+    scommand cmd = scommand_new();
+    arg_kind_t arg_type;
+    char *arg = NULL;
+    bool finish_cmd = false;
+
+    while (!finish_cmd)
+    {
+
+        arg = parser_next_argument(p, &arg_type);
+        if (arg == NULL)
+        {
+            finish_cmd = true;
+            free(arg);
+        }
+        else
+        {
+            switch (arg_type)
+            {
+            case ARG_NORMAL:
+                scommand_push_back(cmd, arg);
+                break;
+            case ARG_INPUT:
+                scommand_set_redir_in(cmd, arg);
+                break;
+            case ARG_OUTPUT:
+                scommand_set_redir_out(cmd, arg);
+                break;
+            }
+        }
+    }
+
+    return cmd;
 }
 
 pipeline parse_pipeline(Parser p)
 {
+    assert(p != NULL && !parser_at_eof(p));
     pipeline result = pipeline_new();
     scommand cmd = NULL;
     bool error = false, another_pipe = true;
 
-    cmd = parse_scommand(p);
-    error = (cmd == NULL); /* Comando inválido al empezar */
     while (another_pipe && !error)
     {
-        /*
-         * COMPLETAR
-         *
-         */
+        cmd = parse_scommand(p);
+        parser_op_pipe(p, &another_pipe);
+        pipeline_push_back(result, cmd);
+        error = (cmd == NULL); /* Comando inválido al empezar */
     }
-    /* Opcionalmente un OP_BACKGROUND al final */
 
-    /*
-     *
-     * COMPLETAR
-     *
-     */
+    // Seteo si se ejecuta en background o foreground
+    bool was_op_background;
+    parser_op_background(p, &was_op_background);
+    pipeline_set_wait(result, was_op_background);
+
+    bool garbage;
+    parser_garbage(p, &garbage);
+    if (garbage)
+    {
+        char *garbage_str = parser_last_garbage(p);
+        printf("Se borro la siguiente basura: '%s'\n", garbage_str);
+    }
+
+    // TIENE UN BYTE DE MEMORYLIKE
 
     /* Tolerancia a espacios posteriores */
     /* Consumir todo lo que hay inclusive el \n */
     /* Si hubo error, hacemos cleanup */
 
-    return NULL; // MODIFICAR
+    return result; // MODIFICAR
 }
