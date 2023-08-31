53342539005191b709a512996308e5f41f873132
vmatiasw <matias.viola@mi.unc.edu.ar>
2023-08-31 19:51:01 -0300

    Finalizado! espero

--- builtin.c
+++ builtin.c
@@ -9,10 +9,10 @@
 #include "command.h"
 #include "builtin.h"
 
-static const char *internal_commands[] = {"cd", "echo", "exit"};
+static const char *internal_commands[] = {"cd", "help", "exit"};
 
 
-// Funciones de comandos internos:
+// Funciones auxiliares de comandos internos:
 static unsigned int internal_length(const char *internal_commands[]) // Devuelve el numero de comandos internos
 {
     unsigned int length = 0;
@@ -40,10 +40,9 @@ static bool is_internal(char *comando) // Devuelve true si es un comando interno
     return b;
 }
 
-
 // Funciones para la ejecucion de comandos internos: 
 static void f_cd(scommand args){
-    char *directory = scommand_to_string(args); 
+    char *directory = sc_to_string(args); 
     int result = chdir(directory);
     if (result < 0)
     {
@@ -52,11 +51,16 @@ static void f_cd(scommand args){
     }
 }
 
-static void f_echo(scommand args){
-    char *ret = scommand_to_string(args); 
-    printf("%s\n", ret);
+static void f_help(void){
+    printf("Shell: MyBash\n");
+    printf("Autores: Ramiro, Matias, Mora, Daian.\n");
+    printf("Comandos Internos:\n");
+    printf("cd: Para cambiar el directorio actual de trabajo en el sistema de archivos.\n");
+    printf("help: Proporciona informacion al usuario sobre los comandos disponibles.\n");
+    printf("exit: El programa finaliza de forma correcta.\n");
 }
- 
+// Debe mostrar un mensaje por la salida estándar indicando el nombre del shell, el nombre de sus autores 
+// y listar los comandos internos implementados con una breve descripción de lo que hace cada uno.
 
 // Funciones del TAD:
 bool builtin_is_internal(scommand cmd){
@@ -85,8 +89,8 @@ void builtin_run(scommand cmd) {
 
     if (strcmp(comando, "cd") == 0) {
         f_cd(cmd);
-    } else if (strcmp(comando, "echo") == 0) {
-        f_echo(cmd);
+    } else if (strcmp(comando, "help") == 0) {
+        f_help();
     } else if (strcmp(comando, "exit") == 0) {
         exit(0);
     }
