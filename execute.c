b10e1092071c2958ad160723309b630098057f6d
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-09-02 16:16:03 -0300

    inicio de la implementacion de execute.c

---
+++ execute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "execute.h"
+#include "command.h"
+#include "builtin.h"
+
+void execute_pipeline(pipeline apipe)
+{
+    printf("Se esta ejecutando.\n");
+}
