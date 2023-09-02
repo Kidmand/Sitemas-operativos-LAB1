52498d620af4eb0b0f21376b2a10d17286b3e9bb
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-09-02 19:47:07 -0300

    nueva impelementacion de execute

--- execute.c
+++ execute.c
@@ -1,24 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "execute.h"
 #include "command.h"
 #include "builtin.h"
 
-void execute_pipeline(pipeline apipe)
+static void print_execute_error(char *message)
 {
-    // reconocemos entre comandos internos y externos
-    scommand sc = pipeline_front(apipe);
+    printf("ERROR en el execute: %s\n", message);
+}
 
-    bool is_internal = builtin_is_internal(sc);
+static void extern_run(scommand cmd)
+{
+    assert(cmd != NULL);
+    // Obtengo los argumentos
+    char **argv = scommand_to_argv(cmd);
 
-    if (is_internal) // función interna
+    execvp(argv[0], argv); // Ejecuta el proceso.
+
+    // Si sigue ejecutando es porque fallo
+
+    print_execute_error("Fallo la ejecucion del proceso.");
+}
+
+static void execute_single_cmd(scommand cmd)
+{
+    assert(cmd != NULL);
+    bool is_internal = builtin_is_internal(cmd);
+    if (is_internal)
     {
-        builtin_run(sc);
+        builtin_run(cmd);
     }
     else
     {
-        printf("Es un comando externo, todavia no esta implementado.\n");
+        extern_run(cmd);
+    }
+}
+
+void execute_pipeline(pipeline apipe)
+{
+    assert(apipe != NULL);
+    scommand cmd;
+    // bool is_background = pipeline_get_wait(apipe);
+
+    while (pipeline_length(apipe) > 0)
+    {
+        pid_t pc_id_fork = fork();
+        if (pc_id_fork == 0)
+        {
+            cmd = pipeline_front(apipe);
+            execute_single_cmd(cmd);
+            pipeline_pop_front(apipe);
+        }
+        if (pc_id_fork > 0)
+        {
+            pid_t wait_response = wait(NULL);
+            if (wait_response == -1)
+                print_execute_error("Ocurrio un error al ejecutar el wait()\n");
+        }
+        else
+        {
+            print_execute_error("Ocurrio un error al ejecutar el fork()\n");
+        }
     }
 }
