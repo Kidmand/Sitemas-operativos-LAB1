4a379a20a696b5fcb0724c196ef5107fe1954057
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2023-09-02 17:33:47 -0300

    Se agrego el manejo de algunos errores

--- builtin.c
+++ builtin.c
@@ -5,12 +5,17 @@
 #include <assert.h>
 #include <unistd.h>
 
-
 #include "command.h"
 #include "builtin.h"
 
 static const char *internal_commands[] = {"cd", "help", "exit"};
 
+/* Imprime por pantalla el error que se mande por "message"
+ */
+static void print_internal_cmf_error(char *message)
+{
+    printf("Se encontro un error en los comandos internos: %s\n", message);
+}
 
 // Funciones auxiliares de comandos internos:
 static unsigned int internal_length(const char *internal_commands[]) // Devuelve el numero de comandos internos
@@ -40,18 +45,19 @@ static bool is_internal(char *comando) // Devuelve true si es un comando interno
     return b;
 }
 
-// Funciones para la ejecucion de comandos internos: 
-static void f_cd(scommand args){
-    char *directory = scommand_front(args); 
+// Funciones para la ejecucion de comandos internos:
+static void f_cd(scommand args)
+{
+    char *directory = scommand_front(args);
     int result = chdir(directory);
     if (result < 0)
     {
-        fprintf(stderr, "Error al cambiar el directorio de trabajo\n");
-        exit(EXIT_FAILURE);
+        print_internal_cmf_error("Error al cambiar el directorio de trabajo\n");
     }
 }
 
-static void f_help(void){
+static void f_help(void)
+{
     printf("Shell: MyBash\n");
     printf("Autores: Ramiro, Matias, Mora, Daian.\n");
     printf("Comandos Internos:\n");
@@ -59,11 +65,17 @@ static void f_help(void){
     printf("help: Proporciona informacion al usuario sobre los comandos disponibles.\n");
     printf("exit: El programa finaliza de forma correcta.\n");
 }
-// Debe mostrar un mensaje por la salida estándar indicando el nombre del shell, el nombre de sus autores 
+// Debe mostrar un mensaje por la salida estándar indicando el nombre del shell, el nombre de sus autores
 // y listar los comandos internos implementados con una breve descripción de lo que hace cada uno.
 
+static void f_exit(void)
+{
+    exit_mybash = true;
+}
+
 // Funciones del TAD:
-bool builtin_is_internal(scommand cmd){
+bool builtin_is_internal(scommand cmd)
+{
 
     assert(!scommand_is_empty(cmd));
 
@@ -72,14 +84,16 @@ bool builtin_is_internal(scommand cmd){
     return is_internal(comando);
 }
 
-bool builtin_alone(pipeline p) {
+bool builtin_alone(pipeline p)
+{
 
     assert(!pipeline_is_empty(p));
-    
+
     return pipeline_length(p) == 1 && builtin_is_internal(pipeline_front(p));
 }
 
-void builtin_run(scommand cmd) {
+void builtin_run(scommand cmd)
+{
 
     assert(builtin_is_internal(cmd));
 
@@ -87,11 +101,16 @@ void builtin_run(scommand cmd) {
 
     scommand_pop_front(cmd); // cmd solo contiene los argumentos ahora
 
-    if (strcmp(comando, "cd") == 0) {
+    if (strcmp(comando, "cd") == 0)
+    {
         f_cd(cmd);
-    } else if (strcmp(comando, "help") == 0) {
+    }
+    else if (strcmp(comando, "help") == 0)
+    {
         f_help();
-    } else if (strcmp(comando, "exit") == 0) {
-        exit(0);
+    }
+    else if (strcmp(comando, "exit") == 0)
+    {
+        f_exit();
     }
 }
